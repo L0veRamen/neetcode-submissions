8d12ad4156f01f10b0363a5cff96844290819bc7
Chen Qian <53376484+L0veRamen@users.noreply.github.com>
2026-05-09 13:54:28 -0400

    Add: merge-sorted-array - submission-0

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+        while (p2 >= 0) { // nums2 还有要合并的元素
+            // 如果 p1 < 0，那么走 else 分支，把 nums2 合并到 nums1 中
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p--] = nums1[p1--]; // 填入 nums1[p1]
+            } else {
+                nums1[p--] = nums2[p2--]; // 填入 nums2[p2]
+            }
+        }
+    }
+}
