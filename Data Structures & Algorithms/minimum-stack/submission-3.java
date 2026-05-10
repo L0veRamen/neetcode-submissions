7ee959e92fe3acb8ff62329e4c26158738a5d5a4
Chen Qian <53376484+L0veRamen@users.noreply.github.com>
2026-05-10 14:32:33 -0400

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.java
@@ -0,0 +1,25 @@
+class MinStack {
+
+    private final Deque<int[]> st = new ArrayDeque<>();
+
+    public MinStack() {
+        // each int[] {value, currentMin}
+        st.push(new int[] { 0, Integer.MAX_VALUE });
+    }
+
+    public void push(int val) {
+        st.push(new int[] { val, Math.min(getMin(), val) });
+    }
+
+    public void pop() {
+        st.pop();
+    }
+
+    public int top() {
+        return st.peek()[0];
+    }
+
+    public int getMin() {
+        return st.peek()[1];
+    }
+}
