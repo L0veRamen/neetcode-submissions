c7a2b58e75a482390d53f01caadba4fdfa31e82b
Chen Qian <53376484+L0veRamen@users.noreply.github.com>
2026-05-14 19:17:18 -0400

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            int t = temperatures[i];
+            while (!stack.isEmpty() && temperatures[stack.peek()] <= t) {
+                stack.pop();
+            }
+            res[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+            stack.push(i);
+        }
+        return res;
+    }
+}
