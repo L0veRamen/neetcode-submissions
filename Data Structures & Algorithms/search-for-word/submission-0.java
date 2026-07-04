55d7380ca9e169b1fcdbb09d7a6075d674bd7bc5
Chen Qian <53376484+L0veRamen@users.noreply.github.com>
2026-07-04 15:30:32 -0400

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    private int[][] DIRS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    public boolean exist(char[][] board, String word) {
+        char[] w = word.toCharArray();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (dfs(i, j, 0, board, w)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(int i, int j, int k, char[][] board, char[] word) {
+        if (board[i][j] != word[k]) {
+            return false;
+        }
+        if (k == word.length - 1) {
+            return true;
+        }
+        board[i][j] = 0; // mark as visited
+        for (int[] d : DIRS) {
+            {
+                int x = i + d[0];
+                int y = j + d[1];
+                if (x >= 0 && x < board.length && y >= 0 && y < board[x].length
+                    && dfs(x, y, k + 1, board, word)) {
+                    return true;
+                }
+            }
+        }
+        board[i][j] = word[k]; // backtrack pop
+        return false;
+    }
+}
