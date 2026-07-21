9936d3f2802d25370e4e98c8faa488fc45d44dc1
Chen Qian <53376484+L0veRamen@users.noreply.github.com>
2026-07-20 20:39:24 -0400

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+        for (String s : strs) {
+            int[] count = new int[26];
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+            String key = Arrays.toString(count);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
+        }
+        // 哈希表的所有 value 就是分组结果
+        return new ArrayList<>(res.values());
+    }
+}
