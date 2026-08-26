07a6a924243cfdb53e179091430a0180c0099bb3
Chen Qian <53376484+L0veRamen@users.noreply.github.com>
2026-08-26 16:55:47 -0400

    Add: best-time-to-buy-and-sell-stock-ii - submission-4

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        # memo[i][0] -> hold = False, memo[i][1] -> hold = True
+        memo = [[None, None] for _ in range(n)]
+
+        def dfs(i: int, hold: bool) -> int:
+            if i < 0:
+                return -float('inf') if hold else 0
+            
+            # Python treats False as 0 and True as 1 for indexing
+            hold_idx = 1 if hold else 0
+            
+            if memo[i][hold_idx] is not None:
+                return memo[i][hold_idx]
+
+            if hold:
+                res = max(dfs(i - 1, True), dfs(i - 1, False) - prices[i])
+            else:
+                res = max(dfs(i - 1, False), dfs(i - 1, True) + prices[i])
+
+            memo[i][hold_idx] = res
+            return res
+
+        return dfs(n - 1, False)
